0400d9d4e82171e00ef174326c8119cae8065489
deadshadow99 <97075743+deadshadow99@users.noreply.github.com>
2022-05-21 00:39:30 -0500

    Update parachute.py

--- jumper/game/parachute.py
+++ jumper/game/parachute.py
@@ -1,2 +1,60 @@
+import random
+from game.word import words
+
+
+class Parachute:
+    
+
+    def __init__(self):
+      self.word = words
+      self.guess = ""
+      self.reveal = list(len(self.word)*'_')
+      self.lives = 4
+      self.won = False
+      self.lose = False
+      self.jumper ={
+                    
+0: """
+            ___  
+           /___\ 
+           \   / 
+            \ /               
+             0   
+            /|\  
+            / \  
+          
+           ^^^^^^^""",
+
+1:"""                 
+           /___\ 
+           \   / 
+            \ /  
+             0   
+            /|\  
+            / \  
+                    
+           ^^^^^^^""",
+                   
+2:"""          
+           \   / 
+            \ /  
+             0   
+            /|\  
+            / \  
+          ^^^^^^^""",
+
+3:"""          
+            \ /  
+             0   
+            /|\  
+            / \  
+          ^^^^^^^""",
+
+4:"""
+             x   
+            /|\  
+            / \  
+          
+          ^^^^^^^"""}
 
 
