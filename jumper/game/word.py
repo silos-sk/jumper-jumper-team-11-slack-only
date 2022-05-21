69644af4efc836b41bcf9a105688824dec5d31b3
RRIVAS2 <ricardorl1990@gmail.com>
2022-05-21 13:42:46 -0600

    Created for Class, created word instance. Needs corrections

--- jumper/game/word.py
+++ jumper/game/word.py
@@ -1,19 +1,37 @@
 import random
 
-display_list = ["abruptly", "absurd", "abyss", "affix", "askew", "avenue", "awkward", "axiom",
-"azure", "bagpipes", "bandwagon", "banjo", "bayou", "beekeeper", "bikini", "blitz", "blizzard", "boggle",
-"bookworm", "boxcar", "boxful", "buckaroo", "buffalo", "buffoon", "buxom", "buzzard", "buzzing",
-"buzzwords", "caliph", "cobweb", "cockiness", "croquet", "crypt", "curacao", "cycle", "daiquiri",
-"dirndl", "disavow", "dizzying", "duplex", "dwarves", "embezzle", "equip", "espionage", "euouae",
-"exodus", "faking", "fishhook", "fixable", "fjord", "flapjack", "gabby", "galaxy", "galvanize", "gazebo",
-"giaour", "haiku", "haphazard", "hyphen", "iatrogenic", "icebox", "jackpot", "jaundice", "jawbreaker",
-"jaywalk", "jazziest", "kayak", "kazoo", "keyhole", "khaki", "kilobyte", "larynx", "lengths", "lucky",
-"luxury", "lymph", "marquis", "matrix", "megahertz", "microwave", "mnemonic", "naphtha", "nightclub",
-"nowadays", "numbskull", "nymph", "onyx", "ovary", "oxidize", "oxygen", "pajama", "phlegm",
-"pixel", "pizazz", "quartz", "queue", "quips", "quixotic", "quiz", "rhubarb", "rhythm",
-"rickshaw", "schnapps", "scratch", "shiv", "snazzy", "sphinx", "thriftless", "thumbscrew", "topaz",
-"transcript", "transgress", "unknown", "unworthy", "unzip", "uptown", "vaporize", "vixen", "vodka",
-"voodoo", "vortex", "walkway", "waltz", "wave", "wavy", "wyvern", "xylophone", "yachtsman", "yippee", "yoked",
-"youthful", "yummy", "zephyr", "zigzag", "zigzagging", "zilch", "zombie"]  
-words = random.choice(display_list).upper()
+class Word:
+    """
+    Secret word hidden from the player. The secret word is randomly chosen from a list of words.
+
+    """
+
+    def __init__(self):
+        """
+        Constructs a new Word.
+        
+        Args:
+            self (Word) : An instance of Word.
+
+        """
+        word_list = ["abruptly", "absurd", "abyss", "affix", "askew", "avenue", "awkward", "axiom",
+        "azure", "bagpipes", "bandwagon", "banjo", "bayou", "beekeeper", "bikini", "blitz", "blizzard", "boggle",
+        "bookworm", "boxcar", "boxful", "buckaroo", "buffalo", "buffoon", "buxom", "buzzard", "buzzing",
+        "buzzwords", "caliph", "cobweb", "cockiness", "croquet", "crypt", "curacao", "cycle", "daiquiri",
+        "dirndl", "disavow", "dizzying", "duplex", "dwarves", "embezzle", "equip", "espionage", "euouae",
+        "exodus", "faking", "fishhook", "fixable", "fjord", "flapjack", "gabby", "galaxy", "galvanize", "gazebo",
+        "giaour", "haiku", "haphazard", "hyphen", "iatrogenic", "icebox", "jackpot", "jaundice", "jawbreaker",
+        "jaywalk", "jazziest", "kayak", "kazoo", "keyhole", "khaki", "kilobyte", "larynx", "lengths", "lucky",
+        "luxury", "lymph", "marquis", "matrix", "megahertz", "microwave", "mnemonic", "naphtha", "nightclub",
+        "nowadays", "numbskull", "nymph", "onyx", "ovary", "oxidize", "oxygen", "pajama", "phlegm",
+        "pixel", "pizazz", "quartz", "queue", "quips", "quixotic", "quiz", "rhubarb", "rhythm",
+        "rickshaw", "schnapps", "scratch", "shiv", "snazzy", "sphinx", "thriftless", "thumbscrew", "topaz",
+        "transcript", "transgress", "unknown", "unworthy", "unzip", "uptown", "vaporize", "vixen", "vodka",
+        "voodoo", "vortex", "walkway", "waltz", "wave", "wavy", "wyvern", "xylophone", "yachtsman", "yippee", "yoked",
+     "youthful", "yummy", "zephyr", "zigzag", "zigzagging", "zilch", "zombie"]  
+
+        self._secret_word =  word_list[random.randint(0, len(word_list)-1)]
+        
+
+
  
